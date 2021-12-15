0583031c772c29418e842045a610b371e49c0884
dinet <dinet@LAPTOP-IBP754UP>
2021-12-15 13:00:20 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            Console.WriteLine("Sayı1 : " + sayi1 + " Sayı2 : " + sayi2);
+            //sayi1 30
+            //değer tip
+            //stack
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            Console.WriteLine(sayilar1[0]);
+            //sayilar1[0] 999
+            //referans tip
+            //heap
+
+        }
+    }
+}
